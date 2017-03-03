d09cc250ecf4dc6f48616998c7e8b8e81c66e9d6
dominik <dominik.kolesar@gmail.com>
2017-03-03 08:46:11 +0100

    commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+
+
+const API_KEY='AIzaSyAUFrx6kE9sk5UDz7g2xvDXGgM9HyznHeA';
+
+
+class App extends Component {
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo:null
+        };
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos:videos,
+                selectedVideo:videos[0]
+            });
+
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term)=>{this.videoSearch(term)},300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange = {videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
+            </div>
+        );
+    }
+}
+
+
+
+ReactDOM.render(
+    <App />
+  , document.querySelector('.container')
+);
